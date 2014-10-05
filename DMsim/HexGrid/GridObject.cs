d023e37e7c0365ffa63b44a35b7a53597181d05e
Thijs <t.miedema@live.nl>
2014-10-05 13:34:24 +0200

    Added Hexgrid Classes

---
+++ DMsim/HexGrid/GridObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grid.HexGrid
+{
+    class GridObject
+    {
+        public HexVector Position;
+        public int Clearance; //I'm planning on using this for "Clearance level"
+
+        public int getDistance(GridObject other)
+        {
+            return (Position - other.Position).Length;
+        }
+    }
+}
